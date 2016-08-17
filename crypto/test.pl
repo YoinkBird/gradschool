e73bbde8f1ac20de6b1118161b66e169540472ce
yoinkbird <YoinkBird@users.noreply.github.com>
2016-08-16 22:11:12 -0500

    crypto: progress commit

---
+++ crypto/test.pl
@@ -0,0 +1,11 @@
+
+%paper = (
+  "h1:Intro" => {
+    "h2:role" => {
+
+      Wired Equivalent Privacy (WEP) is a deprecated protocol intended to secure wireless networks.
+      However, it is no longer recommended for use as its security mechanism is easily bypassed.
+    }
+  }
+);
+return
