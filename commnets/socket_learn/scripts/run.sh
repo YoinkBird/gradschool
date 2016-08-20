23c6197de6e0e453aa854a5ab8006668f54f361d
YoinkBird <leewatson1@gmail.com>
2016-08-20 16:17:41 -0500

    script: enabling other testing techniques

--- commnets/socket_learn/scripts/run.sh
+++ commnets/socket_learn/scripts/run.sh
@@ -9,20 +9,24 @@ java EchoServer $port &
 sleep 1
 lsof -i :${port}
 
-# named pipe
-# http://serverfault.com/a/297095 
-inputPipe="/tmp/javaEchoClient_input"
-mkfifo /tmp/javaEchoClient_input
-cat > /tmp/javaEchoClient_input &
-echo $! > /tmp/javaEchoClient_input-cat-pid
-cat /tmp/javaEchoClient_input | java EchoClient $host $port &
 
-for i in {0..5}; do 
-  testVal=test${i};
-  echo $testVal > $inputPipe
-done
-echo "TERMINATE" > $inputPipe
-# resolves buffer issues in which java output appears after everything is done
+usepipe=1
+if [[ $usepipe -eq 1 ]];then
+  # named pipe
+  # http://serverfault.com/a/297095 
+  inputPipe="/tmp/javaEchoClient_input"
+  mkfifo /tmp/javaEchoClient_input
+  cat > /tmp/javaEchoClient_input &
+  echo $! > /tmp/javaEchoClient_input-cat-pid
+  cat /tmp/javaEchoClient_input | java EchoClient $host $port &
+
+  for i in {0..5}; do
+    testVal=test${i};
+    echo $testVal > $inputPipe
+  done
+  echo "TERMINATE" > $inputPipe
+  # resolves buffer issues in which java output appears after everything is done
+fi
 sleep 2
 jobs
 echo "done"
