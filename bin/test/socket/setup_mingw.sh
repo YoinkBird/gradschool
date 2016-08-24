192f76d005358c3287882703da979257b217ca71
YoinkBird <leewatson1@gmail.com>
2016-08-24 15:22:02 -0500

    script: setup - checking for gradle, fixing java

--- bin/test/socket/setup_mingw.sh
+++ bin/test/socket/setup_mingw.sh
@@ -1,3 +1,4 @@
 #!/bin/sh -xu
 
-export PATH=$PATH:"/c/Program Files/Java/jdk1.8.0_45/bin"
+export JAVA_HOME="/c/Program Files/Java/jdk1.8.0_45"
+export PATH=$PATH:"$JAVA_HOME/bin"
--- bin/test/socket/setup_verify.sh
+++ bin/test/socket/setup_verify.sh
@@ -1,8 +1,22 @@
-#!/bin/sh -xu
+#!/bin/sh -u
 
-which javac
-rc=$?
-if [[ $rc -ne 0 ]]; then
-  echo "-E- could not find javac, exiting"
-  exit $rc
-fi
+err=0
+missingToolArr=("")
+tools=(
+"javac"
+"gradle"
+)
+for tool in ${tools[@]}; do
+  which $tool
+  rc=$?
+  if [[ $rc -ne 0 ]]; then
+    echo "-E- could not find $tool"
+    err+=1
+    missingToolArr+=($tool)
+  fi
+done
+
+echo "$err tools not found"
+for tool in ${missingToolArr[@]}; do
+  echo $tool
+done
