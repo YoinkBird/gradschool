a8f9a8d7de2b0c0bda8683705ea3d6cf32560fa9
yoinkbird <YoinkBird@users.noreply.github.com>
2016-10-08 00:37:53 -0500

    pandas example - removing right spine

--- commnets/assign_2/pandas_example.py
+++ commnets/assign_2/pandas_example.py
@@ -6,16 +6,17 @@ from pandas import DataFrame
 df = DataFrame(np.random.randn(5, 3), columns=['A', 'B', 'C'])
 
 fig, ax = plt.subplots()
-ax2, ax3 = ax.twinx(), ax.twinx()
-rspine = ax3.spines['right']
-rspine.set_position(('axes', 1.25))
-ax3.set_frame_on(True)
-ax3.patch.set_visible(False)
+#ax2, ax3 = ax.twinx(), ax.twinx()
+ax2 = ax.twinx()
+#rspine = ax3.spines['right']
+#rspine.set_position(('axes', 1.25))
+#ax3.set_frame_on(True)
+#ax3.patch.set_visible(False)
 fig.subplots_adjust(right=0.75)
 
 df.A.plot(ax=ax, style='b-')
 df.B.plot(ax=ax2, style='r-', secondary_y=True)
-df.C.plot(ax=ax3, style='g-')
+#df.C.plot(ax=ax3, style='g-')
 
 
 plt.show()
