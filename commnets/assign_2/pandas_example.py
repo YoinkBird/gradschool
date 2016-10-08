3849ab7ec561818efdb8381442e58071f62ea232
yoinkbird <YoinkBird@users.noreply.github.com>
2016-10-08 00:37:53 -0500

    pandas example

---
+++ commnets/assign_2/pandas_example.py
@@ -0,0 +1,21 @@
+# http://stackoverflow.com/questions/11640243/pandas-plot-multiple-y-axes
+
+import matplotlib.pyplot as plt
+import numpy as np
+from pandas import DataFrame
+df = DataFrame(np.random.randn(5, 3), columns=['A', 'B', 'C'])
+
+fig, ax = plt.subplots()
+ax2, ax3 = ax.twinx(), ax.twinx()
+rspine = ax3.spines['right']
+rspine.set_position(('axes', 1.25))
+ax3.set_frame_on(True)
+ax3.patch.set_visible(False)
+fig.subplots_adjust(right=0.75)
+
+df.A.plot(ax=ax, style='b-')
+df.B.plot(ax=ax2, style='r-', secondary_y=True)
+df.C.plot(ax=ax3, style='g-')
+
+
+plt.show()
