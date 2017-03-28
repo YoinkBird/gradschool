f8161c089ce3a81a0609c283689986629f421dd0
YoinkBird <leewatson1@gmail.com>
2017-03-28 14:39:20 -0500

    islr 4.10: draft - working confusion_matrix

--- datamine/hw3/islr_4-10.py
+++ datamine/hw3/islr_4-10.py
@@ -14,10 +14,16 @@ from sklearn.preprocessing import PolynomialFeatures
 from sklearn.pipeline import make_pipeline
 from sklearn import model_selection, cross_validation
 
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.metrics import confusion_matrix, classification_report, precision_score
+
+import statsmodels.api as sm
+import statsmodels.formula.api as smf
+
 # %matplotlib inline
 datafile="../input/islr_data/Weekly.csv"
 datafile="../input/islr_data/Smarket.csv"
-data = pd.read_csv(datafile,index_col=0)
+data = pd.read_csv(datafile,index_col=0, usecols=range(1,10), parse_dates=True)
 
 # preprocessing
 #data = pd.concat([data,pd.get_dummies(data[['default','student']])],axis=1)
@@ -33,6 +39,12 @@ print(data.info())
 seed = 42
 np.random.seed(seed)
 
+'''
+This question should be answered using the Weekly data set, which is part of the ISLR package.
+This data is similar in nature to the Smarket data from this chapter's lab,
+ except that it contains 1089 weekly returns for 21 years, from the beginning of 1990 to the end of 2010.
+Note: Smarket is 2001-2005
+'''
 print("Produce some numerical and graphical summaries of the Weekly data. Do there appear to be any patterns?")
 data.hist()
 data.corr().plot() # TODO: seaborn
@@ -41,30 +53,96 @@ plt.show()
 # plot volume over "index", which loosely corresponds to year:
 plt.scatter(data.index,data['volume'])
 plt.show()
-plt.plot(data.index,data['year'])
-plt.show()
+#plt.plot(data.index,data['year'])
+#plt.show()
 print("""
 Use the full data set to perform a logistic regression with Direction as the response and the five lag variables plus Volume as predictors. 
 Use the summary function to print the results.
 Do any of the predictors appear to be statistically significant? If so, which ones?
 """)
-if(0): # not working yet
-  model = make_pipeline(LogisticRegression()) #Ridge())
-  if(1):
-    xcolname = 'direction'
-    x = data['xcolname'].values.reshape(-1,1)
+predictors = ['lag1','lag2','lag3','lag4','lag5','volume']
+if(1): # not working yet
+  y = data['direction']
+  x = sm.add_constant(data[['lag1','lag2','lag3','lag4','lag5','volume']])
+  est = smf.Logit(y,x).fit()
+  print(est.summary())
+  lda2 = LinearDiscriminantAnalysis()
+  pred2 = lda2.fit(x,y).predict(x)
+  print(confusion_matrix(y, pred2))
+  print(classification_report(y,pred2,digits=3))
+  if(0):
+    model = make_pipeline(LogisticRegression()) #Ridge())
+#    xcolname = 'direction'
+#    x = data['xcolname'].values.reshape(-1,1)
+    data.head()
+    x = data[:'2005'][['lag1','lag2','lag3','lag4','lag5']]
+    x.head()
+    y = data[:'2005']['direction']
+    model.fit(x,y)
     x2 = x.copy()
     x2.sort(axis=0)
-    y = data.default.values#.reshape(-1,1)
+    #y = data.default.values#.reshape(-1,1)
     model.fit(x,y)
-    y_pred = model.predict(x2)
+    y_pred = model.predict(x)
     # http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html#sklearn.linear_model.LogisticRegression.predict_proba
-    y_pred = model.predict_proba(x2)
+    y_pred = model.predict_proba(x)
     plt.scatter(x,y,color='teal')
     # y_pred: 	T : array-like, shape = [n_samples, n_classes]
-    plt.plot(x2, y_pred[:,1],color='lightblue')# color=colors[count], marker='.')
+    plt.plot(x, y_pred[:,1],color='lightblue')# color=colors[count], marker='.')
     plt.show()
   print(model.score(x,y))
 else:
   print("-I-: Skipping...")
 
+'''
+(c) Compute the confusion matrix and overall fraction of correct predictions.
+Explain what the confusion matrix is telling you about the types of mistakes made by logistic regression.
+(d) Now fit the logistic regression model using a training data period
+from 1990 to 2008, with Lag2 as the only predictor. Compute the
+confusion matrix and the overall fraction of correct predictions
+for the held out data (that is, the data from 2009 and 2010).
+(e) Repeat (d) using LDA.
+ ---
+ pred = lda.fit(x,y).predict(x)
+ from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+lda = LinearDiscriminantAnalysis
+pred_my = est.predict()
+pred = lda.fit(x,y).predict(x)
+lda.fit(x,y)
+lda.fit(x , y)
+X_train = data[:'2004'][['lag1','lag2']]
+y_train = data[:'2004']['direction']
+lda = LinearDiscriminantAnalysis()
+lda.fit(X_train, y_train)
+pred = lda.fit(X_train,y_train).predict(X_train)
+confusion_matrix(y_train, pred)
+
+lda.fit(x,y)
+lda.fit(x,y).predict()
+lda.fit(x,y).predict(x)
+pred = lda.fit(x,y).predict(x)
+confusion_matrix(y_train, pred)
+confusion_matrix(y_train, pred).T
+y_train.shape
+pred.shape
+pwd
+pwd
+pred = lda.fit(x,y).predict(x)
+ ---
+(f) Repeat (d) using QDA.
+(g) Repeat (d) using KNN with K = 1.
+(h) Which of these methods appears to provide the best results on
+this data?  
+(i) Experiment with different combinations of predictors, includ
+ing possible transformations and interactions, for each of the
+methods. Report the variables, method, and associated confu
+sion matrix that appears to provide the best results on the held
+out data. Note that you should also experiment with values for
+K in the KNN classifier.
+'''
+
+
+## resources
+'''
+http://blog.yhat.com/posts/logistic-regression-python-rodeo.html
+'''
