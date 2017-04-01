ce6e08dbdc9e979bb95868f88f1f79567e01558d
YoinkBird <leewatson1@gmail.com>
2017-04-01 14:08:57 -0500

    islr 4.10: draft - rearrange before resuming work

--- datamine/hw3/islr_4-10.py
+++ datamine/hw3/islr_4-10.py
@@ -39,13 +39,13 @@ print(data.info())
 seed = 42
 np.random.seed(seed)
 
-'''
+print("""
 This question should be answered using the Weekly data set, which is part of the ISLR package.
 This data is similar in nature to the Smarket data from this chapter's lab,
  except that it contains 1089 weekly returns for 21 years, from the beginning of 1990 to the end of 2010.
-Note: Smarket is 2001-2005
-'''
-print("Produce some numerical and graphical summaries of the Weekly data. Do there appear to be any patterns?")
+Note: Weekly is 1990-2010. Smarket is 2001-2005
+""")
+print("(a) Produce some numerical and graphical summaries of the Weekly data. Do there appear to be any patterns?")
 data.hist()
 data.corr().plot() # TODO: seaborn
 plt.show()
@@ -56,18 +56,23 @@ plt.show()
 #plt.plot(data.index,data['year'])
 #plt.show()
 print("""
-Use the full data set to perform a logistic regression with Direction as the response and the five lag variables plus Volume as predictors. 
+(b) Use the full data set to perform a logistic regression with Direction as the response and the five lag variables plus Volume as predictors. 
 Use the summary function to print the results.
 Do any of the predictors appear to be statistically significant? If so, which ones?
 """)
 predictors = ['lag1','lag2','lag3','lag4','lag5','volume']
-if(1): # not working yet
+if(1):
   y_full = data['direction']
   x_full = sm.add_constant(data[predictors])
   est = smf.Logit(y_full,x_full).fit()
   print(est.summary())
   lda_full = LinearDiscriminantAnalysis()
   pred_full = lda_full.fit(x_full,y_full).predict(x_full)
+  print(
+  '''
+  (c) Compute the confusion matrix and overall fraction of correct predictions.
+  Explain what the confusion matrix is telling you about the types of mistakes made by logistic regression.
+  ''')
   print(confusion_matrix(y_full, pred_full))
   print(classification_report(y_full,pred_full,digits=3))
   if(0):
@@ -90,17 +95,18 @@ if(1): # not working yet
     # y_pred: 	T : array-like, shape = [n_samples, n_classes]
     plt.plot(x, y_pred[:,1],color='lightblue')# color=colors[count], marker='.')
     plt.show()
-  print(model.score(x,y))
+    print(model.score(x,y))
 else:
   print("-I-: Skipping...")
 
-'''
-(c) Compute the confusion matrix and overall fraction of correct predictions.
-Explain what the confusion matrix is telling you about the types of mistakes made by logistic regression.
+print('''
 (d) Now fit the logistic regression model using a training data period
-from 1990 to 2008, with Lag2 as the only predictor. Compute the
-confusion matrix and the overall fraction of correct predictions
+from 1990 to 2008, with Lag2 as the only predictor.
+Compute the confusion matrix and the overall fraction of correct predictions
 for the held out data (that is, the data from 2009 and 2010).
+Note: Weekly is 1990-2010. Smarket is 2001-2005. => train:2001-2004 hold:2004-2005
+''')
+print('''
 (e) Repeat (d) using LDA.
  ---
  pred = lda.fit(x,y).predict(x)
@@ -129,17 +135,25 @@ pwd
 pwd
 pred = lda.fit(x,y).predict(x)
  ---
+''')
+print('''
 (f) Repeat (d) using QDA.
+''')
+print('''
 (g) Repeat (d) using KNN with K = 1.
+''')
+print('''
 (h) Which of these methods appears to provide the best results on
 this data?  
+''')
+print('''
 (i) Experiment with different combinations of predictors, includ
 ing possible transformations and interactions, for each of the
 methods. Report the variables, method, and associated confu
 sion matrix that appears to provide the best results on the held
 out data. Note that you should also experiment with values for
 K in the KNN classifier.
-'''
+''')
 
 
 ## resources
