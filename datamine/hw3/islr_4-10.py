905c7ebfa552dc5f1fb0c543b3843588b53ec026
YoinkBird <leewatson1@gmail.com>
2017-04-01 20:43:03 -0500

    islr 4.10: draft - part e - LDA

--- datamine/hw3/islr_4-10.py
+++ datamine/hw3/islr_4-10.py
@@ -87,34 +87,27 @@ Note: Weekly is 1990-2010. Smarket is 2001-2005. => train:2001-2004 hold:2004-20
 ''')
 print('''
 (e) Repeat (d) using LDA.
- ---
- pred = lda.fit(x,y).predict(x)
- from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-lda = LinearDiscriminantAnalysis
-pred_my = est.predict()
-pred = lda.fit(x,y).predict(x)
-lda.fit(x,y)
-lda.fit(x , y)
-X_train = data[:'2004'][['lag1','lag2']]
-y_train = data[:'2004']['direction']
-lda = LinearDiscriminantAnalysis()
-lda.fit(X_train, y_train)
-pred = lda.fit(X_train,y_train).predict(X_train)
-confusion_matrix(y_train, pred)
-
-lda.fit(x,y)
-lda.fit(x,y).predict()
-lda.fit(x,y).predict(x)
-pred = lda.fit(x,y).predict(x)
-confusion_matrix(y_train, pred)
-confusion_matrix(y_train, pred).T
-y_train.shape
-pred.shape
-pwd
-pwd
-pred = lda.fit(x,y).predict(x)
- ---
 ''')
+if(1):
+  # split into test,train datasets
+  train_lim = '2004'
+  test_start  = '2005'
+  predictors = ['lag2']
+  X_train = data[:'2004'][predictors]
+  y_train = data[:'2004']['direction']
+
+  X_test = data[test_start:][predictors]
+  y_test = data[test_start:]['direction']
+
+  lda = LinearDiscriminantAnalysis()
+  y_pred = lda.fit(X_train,y_train).predict(X_test)
+  print("confusion matrix for held out data")
+  # maybe? confusion_matrix(y_train, pred).T
+  print(confusion_matrix(y_test, y_pred))
+  print("overall fraction of correct predictions for the held out data")
+  print(classification_report(y_test,y_pred,digits=3))
+else:
+  print("-I-: Skipping...")
 print('''
 (f) Repeat (d) using QDA.
 ''')
@@ -139,3 +132,24 @@ K in the KNN classifier.
 '''
 http://blog.yhat.com/posts/logistic-regression-python-rodeo.html
 '''
+if(0):
+  model = make_pipeline(LogisticRegression()) #Ridge())
+#    xcolname = 'direction'
+#    x = data['xcolname'].values.reshape(-1,1)
+  data.head()
+  x = data[:'2005'][['lag1','lag2','lag3','lag4','lag5']]
+  x.head()
+  y = data[:'2005']['direction']
+  model.fit(x,y)
+  x2 = x.copy()
+  x2.sort(axis=0)
+  #y = data.default.values#.reshape(-1,1)
+  model.fit(x,y)
+  y_pred = model.predict(x)
+  # http://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html#sklearn.linear_model.LogisticRegression.predict_proba
+  y_pred = model.predict_proba(x)
+  plt.scatter(x,y,color='teal')
+  # y_pred: 	T : array-like, shape = [n_samples, n_classes]
+  plt.plot(x, y_pred[:,1],color='lightblue')# color=colors[count], marker='.')
+  plt.show()
+  print(model.score(x,y))
